2e8f40025fd307be1556953e965944a48d477fa7
Jack Myers <jackhmyers97@gmail.com>
2024-06-22 19:47:09 -0700

    update to latest adaptive cpp

--- acpp/no-install/clone_acpp.sh
+++ acpp/no-install/clone_acpp.sh
@@ -4,5 +4,5 @@ mkdir -p $PIXI_PROJECT_ROOT/acpp-files
 sudo chown -R $(id -un):$(id -un) $PIXI_PROJECT_ROOT/acpp-files
 
 if ! [ -d $PIXI_PROJECT_ROOT/acpp-files/src ]; then
-	git clone --depth=1 https://github.com/AdaptiveCpp/AdaptiveCpp $PIXI_PROJECT_ROOT/acpp-files/src
+	git clone --depth=1 --branch=v24.02.0 https://github.com/AdaptiveCpp/AdaptiveCpp $PIXI_PROJECT_ROOT/acpp-files/src
 fi
